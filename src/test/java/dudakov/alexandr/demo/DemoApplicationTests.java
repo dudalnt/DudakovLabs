b8aff2b70a58cf92a1e069db4016499091849b9e
Dudakov <dudakov.a.i.98@gmail.com>
2020-02-21 10:35:36 +0300

    lab1

---
+++ src/test/java/dudakov/alexandr/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dudakov.alexandr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
